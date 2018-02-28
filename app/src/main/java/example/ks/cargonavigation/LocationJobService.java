3a03f092b1fd512292e3bbd26d59b6ad70800136
Kapil <manish.singaria@gmail.com>
2018-02-28 17:15:33 +0530

    Location Service added

--- app/src/main/java/example/ks/cargonavigation/LocationJobService.java
+++ app/src/main/java/example/ks/cargonavigation/LocationJobService.java
@@ -1,8 +1,86 @@
-package PACKAGE_NAME;
+package example.ks.cargonavigation;
 
-/**
- * Created by SkyLord on 28-02-2018.
- */
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
-public class LocationJobService {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+public class LocationJobService extends JobService implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener,LocationListener {
+
+    private RequestQueue reQueue;
+    private final String url="http://139.59.93.235:5000/loc";
+
+    @Override
+    public boolean onStartJob(JobParameters job) {
+        Log.d("Job Dispatcher", "Job Started");
+        reQueue = Volley.newRequestQueue(this);
+        StringRequest request = new StringRequest(Request.Method.GET,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Volley", "Network Error!");
+                    }
+                });
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters job) {
+        Log.d("Job Dispatcher", "Job Terminated");
+        return false;
+    }
+
+    @Override
+    public void onLocationChanged(Location location){
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle){
+
+    }
+    @Override
+    public void onProviderEnabled(String s){
+
+    }
+    @Override
+    public void onProviderDisabled(String s){
+
+    }
 }
--- app/src/main/java/example/ks/cargonavigation/MainActivity.java
+++ app/src/main/java/example/ks/cargonavigation/MainActivity.java
@@ -3,11 +3,26 @@ package example.ks.cargonavigation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Trigger;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(this));
+        Job locationJob = dispatcher.newJobBuilder()
+                .setService(LocationJobService.class)
+                .setTag("LocationJob")
+                .setRecurring(true)
+                .setTrigger(Trigger.executionWindow(0,60))
+                .setConstraints(Constraint.ON_ANY_NETWORK)
+                .build();
+        dispatcher.mustSchedule(locationJob);
     }
 }
